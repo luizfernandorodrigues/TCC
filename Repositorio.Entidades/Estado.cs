d2ea2d7e13f3a094462c5375ec9d61ee921f3a07
luizfernandorodrigues <luiz@jmti.com.br>
2017-06-20 22:20:12 -0300

    criação do model estado

---
+++ Repositorio.Entidades/Estado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Repositorio.Entidades
+{
+   public class Estado
+    {
+        public int estadoId { get; set; }
+        public string nome { get; set; }
+        public string sigla { get; set; }
+    }
+}
